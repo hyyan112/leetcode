6eda3ff3e3dde330a1d6721930118b844b872f4b
huangyan <yan.huang@fraudmetrix.cn>
2016-05-13 10:17:25 +0800

    Update contains-duplicate i and ii

---
+++ Python/contains-duplicate-ii.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        r = dict()
+        for i, item in enumerate(nums):
+            if r.has_key(item) and i - r[item] <= k:
+                return True
+            else:
+                r[item] = i
+        else:
+            return False
