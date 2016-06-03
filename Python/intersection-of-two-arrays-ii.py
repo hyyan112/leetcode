eb8318ba2b6eea735dd6473e9182f80bbae56591
leafyongzhong <leafyongzhong@gmail.com>
2016-06-03 23:55:17 +0800

    intersection-of-two-arrays-ii

---
+++ Python/intersection-of-two-arrays-ii.py
@@ -0,0 +1,22 @@
+# encoding:utf-8
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1.sort()
+        nums2.sort()
+        a = []
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                a.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        return a
